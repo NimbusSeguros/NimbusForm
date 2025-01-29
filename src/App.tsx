b3d0e7b8010936db317595650df316af84745997
Nawe1337 <155550180+Nawe1337@users.noreply.github.com>
2025-01-29 14:58:45 -0300

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,21 @@
+
+
+import './App.css'
+
+function App() {
+ 
+
+  return (
+    <>
+    <div className='text-black bg-gray-200 p-4 '>
+    <h1 className="text-3xl font-bold underline ">
+    Hello world!
+  </h1>
+  <p className="text-lg  "> asdas</p>
+    </div>
+
+    </>
+  )
+}
+
+export default App
